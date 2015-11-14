80dfde28113af62fadac2f0b0c78940eeaaaf43b
morrissh <morrissh@airproducts.com>
2015-11-14 13:19:53 -0800

    rename module to AdminModule. Create/test database connection

---
+++ iotNet/Database/dbConn.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace iotNet.Database
+{
+    public class dbConn : IDisposable
+    {
+        // members
+        private string connString;
+        private SqlConnection conn;
+        
+        // constructor
+        public dbConn(){
+            connString = ConfigurationManager.ConnectionStrings["ACN"].ConnectionString;
+            string password = ConfigurationManager.AppSettings["dbPassword"];
+            conn = new SqlConnection(connString);
+            conn.Open();
+        }
+        
+        // public functions
+        public string[] getAreaList()
+        {
+            return null;
+        }
+
+        // private helper functions
+
+
+        // Interface implementations
+
+        public void Dispose()
+        {
+            conn.Close();
+        }
+    }
+}
---
+++ iotNet/Modules/AdminModule.cs
@@ -0,0 +1,21 @@
+﻿
+using iotNet.Database;
+using Nancy;
+
+namespace iotNet.Modules
+{
+    public class AdminModule : NancyModule
+    {
+        public AdminModule()
+        {
+
+            Get["/"] = parameters =>
+            {
+                dbConn db = new dbConn();
+                db.Dispose();
+                return View["index"];
+            };
+
+        }
+    }
+}
